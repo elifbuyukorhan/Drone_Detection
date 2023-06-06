723cedcbe5d28d9ad2efd650879ac09baacb7a2d
elifbuyukorhan <elifbuyukorhan@gmail.com>
2023-06-06 13:47:23 +0000

    new tutorials completed.

---
+++ tutorial3/CNN.py
@@ -0,0 +1,151 @@
+import numpy as np
+import pandas as pd
+import torch
+from torchsummary import summary
+import matplotlib.pyplot as plt
+from plot_utils import *
+from plotting import *
+
+plt.style.use('ggplot')
+plt.rcParams.update({'font.size': 16, 'axes.labelweight': 'bold', 'axes.grid': False})
+
+# Convolutions and Filters
+
+image = torch.from_numpy(plt.imread("/home/projects/tutorial3/tom_bw.png"))
+plt.imshow(image, cmap='gray')
+plt.axis('off')
+#plt.savefig("tom-bw")
+'''
+kernel = torch.tensor([[[[ 0.0625,  0.1250,  0.0625],
+                         [ 0.1250,  0.2500,  0.1250],
+                         [ 0.0625,  0.1250,  0.0625]]]])
+plot_conv(image, kernel)
+
+kernel = torch.tensor([[[[ -2,  -1,  0],
+                         [ -1,   1,  1],
+                         [  0,   1,  2]]]])
+plot_conv(image, kernel)
+
+
+kernel = torch.tensor([[[[  -1,  -1,   -1],
+                         [  -1,   8,   -1],
+                         [  -1,  -1,   -1]]]])
+plot_conv(image, kernel)
+
+# Convolutional Layers
+
+# 1 kernel of (3,3)
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size=(5, 5))
+plot_convs(image, conv_layer)
+
+# 2 kernels of (3,3)
+conv_layer = torch.nn.Conv2d(1, 2, kernel_size=(3, 3))
+plot_convs(image, conv_layer)
+
+# 3 kernels of (5,5)
+conv_layer = torch.nn.Conv2d(1, 3, kernel_size=(5, 5))
+plot_convs(image, conv_layer)
+
+# 1 kernel of (51,51)
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size=(50, 50))
+plot_convs(image, conv_layer, axis=True)
+
+# 1 kernel of (51,51) with padding
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size=(51, 51), padding=25)
+plot_convs(image, conv_layer, axis=True)
+
+# 1 kernel of (25,25) with stride of 3
+conv_layer = torch.nn.Conv2d(1, 1, kernel_size=(25, 25), stride=3)
+plot_convs(image, conv_layer, axis=True)
+
+# Flattening
+class CNN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(3, 3), padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(in_channels=3, out_channels=2, kernel_size=(3, 3), padding=1),
+            torch.nn.ReLU(),
+            torch.nn.Flatten(),
+            torch.nn.Linear(20000, 1)
+        )
+
+    def forward(self, x):
+        out = self.main(x)
+        return out
+    
+model = CNN()
+summary(model, (1, 100, 100))
+
+# Pooling
+class CNN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.main = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels=1, out_channels=3, kernel_size=(3, 3), padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d((2, 2)),
+            torch.nn.Conv2d(in_channels=3, out_channels=2, kernel_size=(3, 3), padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d((2, 2)),
+            torch.nn.Flatten(),
+            torch.nn.Linear(1250, 1)
+        )
+
+    def forward(self, x):
+        out = self.main(x)
+        return out
+    
+model = CNN()
+summary(model, (1, 100, 100))
+
+'''
+#CNN vs Fully Connected NN
+
+def linear_block(input_size, output_size):
+    return torch.nn.Sequential(
+        torch.nn.Linear(input_size, output_size),
+        torch.nn.ReLU()
+    )
+
+def conv_block(input_channels, output_channels):
+    return torch.nn.Sequential(
+        torch.nn.Conv2d(input_channels, output_channels, (3, 3), padding=1),
+        torch.nn.ReLU()
+    )
+
+class NN(torch.nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.main = torch.nn.Sequential(
+            linear_block(input_size, 256),
+            linear_block(256, 128),
+            linear_block(128, 64),
+            linear_block(64, 16),
+            torch.nn.Linear(16, 1)
+        )
+        
+    def forward(self, x):
+        out = self.main(x)
+        return out
+    
+class CNN(torch.nn.Module):
+    def __init__(self, input_channels):
+        super().__init__()
+        self.main = torch.nn.Sequential(
+            conv_block(input_channels, 3),
+            conv_block(3, 3),
+            conv_block(3, 3),
+            conv_block(3, 3),
+            conv_block(3, 3),
+            torch.nn.Flatten(),
+            torch.nn.Linear(49152, 1)
+        )
+        
+    def forward(self, x):
+        out = self.main(x)
+        return out
+    
+model = CNN(input_channels=1)
+summary(model, (1, 128, 128))
